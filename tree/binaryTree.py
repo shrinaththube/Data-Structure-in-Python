28faef49b01b5b3a227d266243b6c72177d3b67b
shrinaththube <shrinaththube@gmail.com>
2016-06-08 17:25:32 -0700

    added method to construct new tree as a mirror of given tree.

--- tree/binaryTree.py
+++ tree/binaryTree.py
@@ -35,7 +35,7 @@ class BTOperations(object):
         self.printPostorder(refNode.lChild)
         self.printPostorder(refNode.rChild)
         print refNode.key,
-    
+    @staticmethod
     def displayTree(self,refNode):
         if refNode == None:
             print "Tree is not formed yet"
@@ -96,7 +96,20 @@ class BTOperations(object):
             
         self.makeMirrorTree(refNode.lChild) # recursive call to left subtree
         self.makeMirrorTree(refNode.rChild) # recursive call to right subtree
+    
+    ''' This will construct new mirror tree without changing original tree'''
+    def createNewMirrorTree(self,refNode):
+        if refNode == None: return # breaking condition of recursion
         
+        rootNode = BTNode(refNode.key) # create new binary tree node
+        
+        if refNode.lChild == None and refNode.rChild == None: #return leaf node - breaking condition
+            return rootNode
+        
+        rootNode.lChild = self.createNewMirrorTree(refNode.lChild) #recursive call
+        rootNode.rChild = self.createNewMirrorTree(refNode.rChild) #recursive call
+        
+        return rootNode #returns root of new tree
     
         
 BTree1 = BTOperations()
@@ -105,7 +118,8 @@ BTree1.root.lChild = BTNode(3)
 BTree1.root.rChild = BTNode(2)
 BTree1.root.rChild.lChild = BTNode(5)
 BTree1.root.rChild.rChild = BTNode(4)
-BTree1.displayTree(BTree1.root)
+print"BTree1 -"
+BTree1.displayTree(BTree1,BTree1.root)
 
 BTree2 = BTOperations()
 BTree2.root = BTNode(1)
@@ -113,7 +127,8 @@ BTree2.root.lChild = BTNode(2)
 BTree2.root.rChild = BTNode(3)
 BTree2.root.lChild.rChild = BTNode(5)
 BTree2.root.lChild.lChild = BTNode(4)
-BTree2.displayTree(BTree2.root)
+print"BTree2 -"
+BTree2.displayTree(BTree2,BTree2.root)
 
 print"BTree1 is mirror of BTree2 - ", BTree1.isTreeMirror(BTree1.root, BTree2.root)
 
@@ -121,4 +136,11 @@ print "Making BTree1 to its mirror - SO BTree1 = BTree2"
 
 BTree1.makeMirrorTree(BTree1.root)
 
-BTree1.displayTree(BTree1.root)
+BTree1.displayTree(BTree1,BTree1.root)
+
+''' Construct BTree3 as a mirror of BTree1'''
+print "Construct BTree3 as a mirror of BTree1"
+BTree3 = BTOperations()
+BTree3.root = BTree3.createNewMirrorTree(BTree1.root)
+print"BTree3 -"
+BTree3.displayTree(BTree3,BTree3.root)
