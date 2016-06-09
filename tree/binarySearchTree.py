60a7078ae95a319427420e799e3ba50c31a4836e
shrinaththube <shrinaththube@gmail.com>
2016-06-09 15:57:06 -0700

    modified BstOperations as a child class of BTOpearations

--- tree/binarySearchTree.py
+++ tree/binarySearchTree.py
@@ -4,17 +4,10 @@ Created on Jun 5, 2016
 @author: Shrinath Thube
 '''
 
-class BstNode(object):
-    '''
-    This is bst node contains key, reference to left and right child
-    '''
-    def __init__(self, key,lChild = None, rChild = None):
-        self.key = key
-        self.lChild = lChild
-        self.rChild = rChild      
+from tree.binaryTree import BTOperations
+from tree.binaryTree import BTNode
 
-
-class BstOperations(object):
+class BstOperations(BTOperations):
     '''
     This class contains create bst, display it, insert node
     '''
@@ -23,56 +16,19 @@ class BstOperations(object):
     
     def insertNode(self,key,refNode):
         if self.root == None:
-            self.root = BstNode(key)
+            self.root = BTNode(key)
             return
         else:
             if key < refNode.key:
                 if refNode.lChild != None:
                     self.insertNode(key,refNode.lChild)
                 else:
-                    refNode.lChild = BstNode(key)
+                    refNode.lChild = BTNode(key)
             else:
                 if refNode.rChild != None:
                     self.insertNode(key, refNode.rChild)
                 else:
-                    refNode.rChild = BstNode(key)
-    
-    def printInorder(self,refNode):
-        if refNode == None: return
-        self.printInorder(refNode.lChild)
-        print refNode.key,
-        self.printInorder(refNode.rChild)
-    
-    def printPreorder(self,refNode):
-        if refNode == None: return
-        print refNode.key,
-        self.printPreorder(refNode.lChild)
-        self.printPreorder(refNode.rChild)
-    
-    def printPostorder(self,refNode):
-        if refNode == None: return
-        self.printPostorder(refNode.lChild)
-        self.printPostorder(refNode.rChild)
-        print refNode.key,
-    
-    def displayTree(self,refNode):
-        if refNode == None:
-            print "Tree is not formed yet"
-            return
-        print
-        print "******************* Displaying Tree *********************"
-        print "Inorder - ",
-        self.printInorder(refNode)
-        print
-        print "Preorder - ",
-        self.printPreorder(refNode)
-        print
-        print "Postorder - ",
-        self.printPostorder(refNode)
-        print 
-        print "******************* End *********************"
-        print
-    
+                    refNode.rChild = BTNode(key)
     def createBst(self,list_key):
         if len(list_key) == 0:
             print "List is empty or invalid"
@@ -95,7 +51,7 @@ class BstExtrafunctions(BstOperations):
         #print inList, preList
         rEle = preList[self.preIndex]
         self.preIndex +=1
-        rootNode = BstNode(rEle)
+        rootNode = BTNode(rEle)
         
         if inStart == inLast:
             return rootNode
@@ -114,7 +70,7 @@ class BstExtrafunctions(BstOperations):
         #Array is scan in reverse order
         rEle = postList[len(postList)-1-self.postIndex]
         self.postIndex +=1
-        rootNode = BstNode(rEle)
+        rootNode = BTNode(rEle)
         
         if inStart == inLast:
             return rootNode
@@ -125,7 +81,46 @@ class BstExtrafunctions(BstOperations):
         rootNode.lChild = self.createTreeFromInPostOrder(inList, postList, inStart, partPoint-1)
         
         return rootNode
+    '''
+    def createFullBinTreeFromPrePostOrder(self,preList,postList,postStart,postLast):
+        if postStart > postLast or len(preList) == self.preIndex :
+            print "braking condition ", postStart, postLast , postList[postStart:postLast+2], self.postIndex 
+            return
+        
+        print preList, self.preIndex, postList, postStart, postLast, 
+        rEle = preList[self.preIndex]
+        self.preIndex +=1
+        rootNode = BTNode(rEle)
+        
+        print rootNode.key, postList[postStart:postLast+1],
+        
 
+        if postStart == postLast:
+            print "return - ", rootNode.key
+            return rootNode
+        
+        partPoint = postList.index(rEle)
+        print "part point -" ,partPoint
+        
+      
+        rootNode.lChild = self.createFullBinTreeFromPrePostOrder(preList, postList, postStart, partPoint-1)
+        print "start right"
+        rootNode.rChild = self.createFullBinTreeFromPrePostOrder(preList, postList, partPoint+1 , postLast)
+        
+        print "return - ",rootNode.key
+        return rootNode
+        '''
+
+
+'''
+tree5 = BstExtrafunctions()
+preL = [1, 2, 4, 8, 9, 5, 3, 6, 7]
+postL = [8, 9, 4, 5, 2, 6, 7, 3, 1]
+tree5.root = tree5.createFullBinTreeFromPrePostOrder(preL, postL, 1, len(postL)-2)        
+tree5.displayTree(tree5.root)
+'''
+        
+        
 def main():
     tree1 = BstOperations()
     tree2 = BstOperations()
@@ -134,8 +129,9 @@ def main():
     tree1.insertNode(15, tree1.root)
     tree1.insertNode(5, tree1.root)
     tree1.insertNode(20, tree1.root)
-    tree2.displayTree(tree2.root)
-    tree1.displayTree(tree1.root)
+    BTOperations.displayTree(tree2, tree2.root)
+    BTOperations.displayTree(tree1, tree1.root)
+    
     
 if __name__ == '__main__':
     main()
