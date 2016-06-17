5d80f3112ddd07a4dc983b25d4ff11ebbcb7e614
shrinaththube <shrinaththube@gmail.com>
2016-06-17 01:55:38 -0700

    added module for string operations

---
+++ leetCodeProblems/stringOperations.py
@@ -0,0 +1,20 @@
+'''
+Created on Jun 16, 2016
+
+@author: Shrinath Thube
+'''
+
+''' Finds common characters of two strings '''
+def intersectionOf2String(str1,str2):
+    intStr = ""
+    flag = [False] * 256
+ 
+    for b in str2:
+        flag[ord(b)] = True
+    
+    for a in str1:
+        if flag[ord(a)]: intStr +=a  
+    
+    return intStr
+
+print intersectionOf2String("defagbc", "abcdefg")
